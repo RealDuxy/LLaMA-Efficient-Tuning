f3c0cd63e27f707e6b54df069c27acff699f350b
RealDuxy <du.xi.yang.cool@gmail.com>
2023-11-17 01:25:35 +0800

    1. 添加分佈式訓練腳本（accelerator）

---
+++ examples/run_sft_1116_distribute.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+accelerate config # 首先配置分布式环境
+
+accelerate launch  ../src/train_bash.py \
+    --stage sft \
+    --do_train \
+    --template chatglm3 \
+    --dataset high_quality_qa,normal_qa,alpaca_gpt4_zh \
+    --dataset_dir ../data \
+    --finetuning_type lora \
+    --output_dir ../checkpoints/1116_exp1 \
+    --max_samples 30000 \
+    --overwrite_cache \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 32 \
+    --lr_scheduler_type cosine \
+    --lora_rank 16 \
+    --lora_alpha 32 \
+    --lora_target all \
+    --logging_steps 10 \
+    --save_steps 500 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 1.0 \
+    --plot_loss \
+    --cutoff_len 2500  \
+    --reserved_label_len 800  \
+    --fp16  \
+    --neft_alpha 5 \
+    --model_name_or_path THUDM/chatglm3-6b
+
+
