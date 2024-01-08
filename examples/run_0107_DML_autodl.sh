40aac904937b3f87d9d8b8a18b7bb908948e48c7
RealDuxy <du.xi.yang.cool@gmail.com>
2024-01-09 03:48:09 +0800

    1. 0107

--- examples/run_0107_DML_autodl.sh
+++ examples/run_0107_DML_autodl.sh
@@ -13,20 +13,20 @@ CUDA_VISIBLE_DEVICES=0 python  ../src/train_bash.py \
     --output_dir ../checkpoints/0107_stage1_spec_ft \
     --preprocessing_num_workers 64 \
     --overwrite_cache \
-    --per_device_train_batch_size 8 \
-    --gradient_accumulation_steps 8 \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 16 \
     --lr_scheduler_type cosine \
     --lora_rank 16 \
     --lora_alpha 32 \
     --lora_target all \
     --logging_steps 10 \
-    --save_steps 500 \
+    --save_steps 212 \
     --learning_rate 2e-5 \
     --num_train_epochs 3.0 \
     --plot_loss \
     --overwrite_output_dir \
     --cutoff_len 2500  \
-    --reserved_label_len 800  \
+    --reserved_label_len 500  \
     --fp16  \
     --neftune_noise_alpha 5 \
     --model_name_or_path THUDM/chatglm3-6b \
@@ -43,8 +43,8 @@ CUDA_VISIBLE_DEVICES=0 python  ../src/train_bash.py \
     --output_dir ../checkpoints/0107_stage2_mix_alpaca_ft \
     --preprocessing_num_workers 64 \
     --overwrite_cache \
-    --per_device_train_batch_size 8 \
-    --gradient_accumulation_steps 8 \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 16 \
     --lr_scheduler_type cosine \
     --adapter_name_or_path ../checkpoints/0107_stage1_spec_ft \
     --lora_rank 16 \
