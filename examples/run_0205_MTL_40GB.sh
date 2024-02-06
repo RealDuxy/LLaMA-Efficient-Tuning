f8c8b45f6e7ef0384847f19a0db5549603f6f04b
RealDuxy <du.xi.yang.cool@gmail.com>
2024-02-06 13:49:07 +0800

    1. 脚本更新

--- examples/run_0205_MTL_40GB.sh
+++ examples/run_0205_MTL_40GB.sh
@@ -19,7 +19,7 @@ CUDA_VISIBLE_DEVICES=0 python  ../src/train_bash.py \
     --lora_alpha 32 \
     --lora_target all \
     --logging_steps 10 \
-    --save_steps 695 \
+    --save_steps 696 \
     --learning_rate 2e-5 \
     --num_train_epochs 3.0 \
     --plot_loss \
