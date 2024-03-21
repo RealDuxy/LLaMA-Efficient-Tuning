f989893064cd0ccf4404ad68127a81a2e079f8f0
RealDuxy <paliss001@outlook.com>
2024-03-21 18:29:39 +0800

    1. 合并脚本

--- examples/train/0320-train-askbob_qa_sft-exp1.sh
+++ examples/train/0320-train-askbob_qa_sft-exp1.sh
@@ -28,5 +28,31 @@ CUDA_VISIBLE_DEVICES=0 python  ../src/train_bash.py \
     --cutoff_len 1700  \
     --fp16 \
     --save_steps 562 \
-    --model_name_or_path /mnt/e/UbuntuFiles/models_saved/Qwen1.5-14B-Chat-GPTQ-Int4 \
+    --model_name_or_path /mnt/e/UbuntuFiles/models_saved/Qwen1.5-14B-Chat-GPTQ-Int4 \ > 0320_sft.log 2>&1
 #    --overwrite_cache \
+deepspeed --num_gpus=4  ../src/train_bash.py \
+    --stage dpo \
+    --do_train \
+    --template chatglm3 \
+    --dataset askbob_qa_comparison  \
+    --dataset_dir ../data \
+    --finetuning_type lora \
+    --output_dir ../checkpoints/0322_chatglm3-stage1_dpo \
+    --adapter_name_or_path  ../checkpoints/0205_stage1_spec_ft \
+    --preprocessing_num_workers 64 \
+    --overwrite_cache \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 8 \
+    --lr_scheduler_type cosine \
+    --lora_rank 16 \
+    --lora_alpha 32 \
+    --lora_target all \
+    --logging_steps 10 \
+    --learning_rate 2e-5 \
+    --num_train_epochs 1.0 \
+    --plot_loss \
+    --overwrite_output_dir \
+    --cutoff_len 1700  \
+    --fp16 \
+    --model_name_or_path /mnt/e/UbuntuFiles/models_saved/chatglm3/ \
+    --deepspeed ../examples/train/v100_ds_config.json \ > 0320_dpo.log 2>&1
