fe58d28d05ffa02996a73a243d001b2941837c10
RealDuxy <paliss001@outlook.com>
2024-04-26 17:49:07 +0800

    1. 添加qwen1.5 askbob测试

--- examples/train/0426/0426-train-askbob_qa_sft-exp1.sh
+++ examples/train/0426/0426-train-askbob_qa_sft-exp1.sh
@@ -7,7 +7,7 @@
 # exp1 qwen14b
 # single gpus single  experiment
 # askbob max_source_length does not have to be 3000, it's just for test.
-CUDA_VISIBLE_DEVICES=0 python  ../../src/train_bash.py \
+CUDA_VISIBLE_DEVICES=0 python  ../src/train_bash.py \
     --stage sft \
     --do_train \
     --template qwen \
@@ -16,10 +16,10 @@ CUDA_VISIBLE_DEVICES=0 python  ../../src/train_bash.py \
     --finetuning_type lora \
     --use_rslora False \
     --val_size 0.05 \
-    --output_dir ../../checkpoints/qwen/0426_askbob_stage1_qwen14b_gptq_int4_exp1 \
+    --output_dir ../checkpoints/qwen/0426_askbob_stage1_qwen14b_gptq_int4_exp1 \
     --preprocessing_num_workers 64 \
-    --per_device_train_batch_size 2 \
-    --per_device_eval_batch_size 2 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 16 \
     --lr_scheduler_type cosine \
     --lora_rank 16 \
@@ -32,8 +32,8 @@ CUDA_VISIBLE_DEVICES=0 python  ../../src/train_bash.py \
     --overwrite_output_dir \
     --cutoff_len 3000  \
     --fp16 \
-    --save_steps 562 \
-    --eval_steps 562 \
+    --save_steps 88 \
+    --eval_steps 88 \
     --evaluation_strategy steps \
     --load_best_model_at_end \
     --model_name_or_path /mnt/e/UbuntuFiles/models_saved/Qwen1.5-14B-Chat-GPTQ-Int4
