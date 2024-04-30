e4dd5fc69de3f63ed800deec1b8b449a047aca0d
RealDuxy <paliss001@outlook.com>
2024-04-30 10:04:15 +0800

    1. 添加reference cls预测

---
+++ examples/train/0426/predict_cls_vanilla_qwen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+MODEL_PATH=/mnt/e/UbuntuFiles/models_saved/Qwen1.5-14B-Chat-GPTQ-Int4
+#MODEL_PATH=/mnt/d/PycharmProjects/models/Qwen1.5-14B-Chat-GPTQ-Int4
+
+# exp1 qwen14b
+# single gpus single  experiment
+# askbob max_source_length does not have to be 3000, it's just for test.
+CUDA_VISIBLE_DEVICES=2 python  ../src/train_bash.py \
+    --stage sft \
+    --do_predict \
+    --template qwen \
+    --dataset reference_classification_cot  \
+    --dataset_dir ../data \
+    --finetuning_type lora \
+    --output_dir ../checkpoints/qwen/vanilla_qwen/ \
+    --logging_steps 5 \
+    --overwrite_output_dir \
+    --overwrite_cache \
+    --cutoff_len 1300  \
+    --preprocessing_num_workers 32 \
+    --per_device_eval_batch_size 2 \
+    --model_name_or_path $MODEL_PATH \
+    --predict_with_generate
