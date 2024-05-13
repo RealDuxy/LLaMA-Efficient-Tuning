9d945d25dfce5075e5ce2cd506ead1b5a195fc1e
RealDuxy <paliss001@outlook.com>
2024-05-13 10:33:08 +0800

    1. 添加数据

---
+++ data/instruction_only_rag/instruction_only_rag.py
@@ -0,0 +1,71 @@
+import json
+import datasets
+from typing import Any, Dict, List
+
+_DESCRIPTION = "RAG dataset"
+_CITATION = ""
+_HOMEPAGE = ""
+_LICENSE = ""
+_URL = "./"
+
+_URLS = {
+    "test": _URL + "train_instruction_only.json",
+    "train": _URL + "eval_instruction_only.json",
+}
+
+
+class InstructionOnlyDataset(datasets.GeneratorBasedBuilder):
+    VERSION = datasets.Version("0.0.0")
+    def _info(self) -> datasets.DatasetInfo:
+        features = datasets.Features({
+            "system": datasets.Value("string"),
+            "instruction": datasets.Value("string"),
+            "input": datasets.Value("string"),
+            "output": datasets.Value("string"),
+            "history": datasets.Sequence(datasets.Sequence(datasets.Value("string")))
+        })
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=features,
+            homepage=_HOMEPAGE,
+            license=_LICENSE,
+            citation=_CITATION
+        )
+
+    def _split_generators(self, dl_manager: datasets.DownloadManager):
+        file_path = dl_manager.download(_URLS)
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={
+                    "filepath": file_path["train"]
+                }
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST,
+                gen_kwargs={
+                    "filepath": file_path["test"]
+                }
+            )
+        ]
+
+    def _generate_examples(self, filepath: str) -> Dict[int, Dict[str, Any]]:
+        example_dataset = json.load(open(filepath, "r", encoding="utf-8"))
+        prompt_templates = json.load(open("template.json", "r", encoding="utf-8"))
+        system = prompt_templates["history"][0]["content"]
+        prompt = prompt_templates["prompt"]
+        for key, example in enumerate(example_dataset):
+            question = example["question"]
+            if question[-1] not in ["？", "。", "！", "?", ".", "!"]:
+                question += "？"
+            requirement = example["requirement"].replace("\n", "")
+            output = example["output"]
+            is_positive = example["is_positive"]
+            new_example = {
+                "system": system,
+                "instruction": prompt.replace("{question}", question).replace("{requirement}", requirement),
+                "input": "",
+                "output": output,
+                "history": []
+            }
+            yield key, new_example
