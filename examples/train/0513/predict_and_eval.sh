81fbecd6e1b0398075e1198b07ac9a3dac6122d3
RealDuxy <paliss001@outlook.com>
2024-05-14 15:36:13 +0800

    1. 脚本提供

--- examples/train/0513/predict_and_eval.sh
+++ examples/train/0513/predict_and_eval.sh
@@ -1,5 +1,5 @@
 # 并行执行 task1 和 task2
-CUDA_VISIBLE_DEVICES=0 python ../src/train.py ../examples/train/0513/predict_exp1.yaml > ../log/predict_exp1.log 2>&1 &
-CUDA_VISIBLE_DEVICES=1 python ../src/train.py ../examples/train/0513/predict_exp2.yaml > ../log/predict_exp2.log 2>&1 &
-CUDA_VISIBLE_DEVICES=2 python ../src/train.py ../examples/train/0513/predict_exp3.yaml > ../log/predict_exp3.log 2>&1 &
-CUDA_VISIBLE_DEVICES=3 python ../src/train.py ../examples/train/0513/predict_exp4.yaml > ../log/predict_exp4.log 2>&1 &
+CUDA_VISIBLE_DEVICES=0 python ../src/train.py ../examples/train/0513/predict_exp1.yaml > log/predict_exp1.log 2>&1 &
+CUDA_VISIBLE_DEVICES=1 python ../src/train.py ../examples/train/0513/predict_exp2.yaml > log/predict_exp2.log 2>&1 &
+CUDA_VISIBLE_DEVICES=2 python ../src/train.py ../examples/train/0513/predict_exp3.yaml > log/predict_exp3.log 2>&1 &
+CUDA_VISIBLE_DEVICES=3 python ../src/train.py ../examples/train/0513/predict_exp4.yaml > log/predict_exp4.log 2>&1 &
