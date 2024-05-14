e65271db137b5b74cf1fa62a20ab1f447b5bd7e3
RealDuxy <paliss001@outlook.com>
2024-05-14 18:06:50 +0800

    1. 脚本提供

--- examples/train/0513/train.sh
+++ examples/train/0513/train.sh
@@ -8,9 +8,13 @@
 #  USE_MODELSCOPE_HUB=1CUDA_VISIBLE_DEVICES=2 python src/train.py examples/train/0513/exp3.yaml > log/exp3.log 2>&1
 #  USE_MODELSCOPE_HUB=1CUDA_VISIBLE_DEVICES=2 python src/train.py examples/train/0513/exp4.yaml > log/exp4.log 2>&1
 #) &
-
-
+echo "estimate gpu memory = 32413mb"
+echo "Start training exp1"
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/0513/exp1.yaml > log/exp1.log 2>&1
-USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=1 python src/train.py examples/train/0513/exp2.yaml > log/exp2.log 2>&1
-USE_MODELSCOPE_HUB=1CUDA_VISIBLE_DEVICES=2 python src/train.py examples/train/0513/exp3.yaml > log/exp3.log 2>&1
-USE_MODELSCOPE_HUB=1CUDA_VISIBLE_DEVICES=2 python src/train.py examples/train/0513/exp4.yaml > log/exp4.log 2>&1
+echo "Start training exp2"
+USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/0513/exp2.yaml > log/exp2.log 2>&1
+echo "Start training exp3"
+USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/0513/exp3.yaml > log/exp3.log 2>&1
+echo "Start training exp4"
+USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/0513/exp4.yaml > log/exp4.log 2>&1
+echo "training complete"
