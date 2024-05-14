156a11983f3d4425950eb7e5f65c4f9af53f327f
RealDuxy <paliss001@outlook.com>
2024-05-14 22:45:49 +0800

    1. 4卡训练脚本

--- examples/train/0513/train.sh
+++ examples/train/0513/train.sh
@@ -9,12 +9,12 @@
 #  USE_MODELSCOPE_HUB=1CUDA_VISIBLE_DEVICES=2 python src/train.py examples/train/0513/exp4.yaml > log/exp4.log 2>&1
 #) &
 echo "estimate gpu memory = 32413mb"
-echo "Start training exp1"
-CUDA_VISIBLE_DEVICES=0,1,2,3 python src/train.py examples/train/0513/exp1.yaml > log/exp1.log 2>&1
 echo "Start training exp2"
 CUDA_VISIBLE_DEVICES=0,1,2,3 python src/train.py examples/train/0513/exp2.yaml > log/exp2.log 2>&1
 echo "Start training exp3"
 CUDA_VISIBLE_DEVICES=0,1,2,3 python src/train.py examples/train/0513/exp3.yaml > log/exp3.log 2>&1
 echo "Start training exp4"
 CUDA_VISIBLE_DEVICES=0,1,2,3 python src/train.py examples/train/0513/exp4.yaml > log/exp4.log 2>&1
+echo "Start training exp1"
+CUDA_VISIBLE_DEVICES=0,1,2,3 python src/train.py examples/train/0513/exp1.yaml > log/exp1.log 2>&1
 echo "training complete"
