47e75a9a5605a8f226d3bbc18d5a003d39a4de56
RealDuxy <paliss001@outlook.com>
2024-05-14 14:37:34 +0800

    1. 脚本提供

---
+++ examples/train/0513/train.sh
@@ -0,0 +1,10 @@
+# 并行执行 task1 和 task2
+CUDA_VISIBLE_DEVICES=0 python ../src/train.py example/train/0513/exp1.yaml > exp1.log 2>&1 &
+sleep 30
+CUDA_VISIBLE_DEVICES=1 python ../src/train.py example/train/0513/exp2.yaml > exp2.log 2>&1 &
+sleep 30
+# 在后台执行串行任务的脚本
+(
+  CUDA_VISIBLE_DEVICES=2 python ../src/train.py example/train/0513/exp3.yaml > exp3.log 2>&1
+  CUDA_VISIBLE_DEVICES=2 python ../src/train.py example/train/0513/exp4.yaml > exp4.log 2>&1
+) &
