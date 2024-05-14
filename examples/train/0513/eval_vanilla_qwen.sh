e93621b764a5b50f3a8bcc1d5f4cf246f029575b
RealDuxy <paliss001@outlook.com>
2024-05-14 15:37:57 +0800

    1. 脚本提供

--- examples/train/0513/eval_vanilla_qwen.sh
+++ examples/train/0513/eval_vanilla_qwen.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=3 python ../src/train.py ../examples/train/0513/predict_qwen.yaml > log/predict_qwen.log 2>&1 &
+CUDA_VISIBLE_DEVICES=3 python src/train.py examples/train/0513/predict_qwen.yaml > log/predict_qwen.log 2>&1 &
