bec5e59885a1c39b69950fb1fc8c25f68bd700c9
RealDuxy <paliss001@outlook.com>
2024-05-16 20:00:48 +0800

    1. 模版修改

--- data/instruction_only_rag/instruction_only_rag.py
+++ data/instruction_only_rag/instruction_only_rag.py
@@ -47,11 +47,9 @@ class InstructionOnlyDataset(datasets.GeneratorBasedBuilder):
             output = example["output"]
             context = example["contexts"]
             is_positive = example["is_positive"]
-            instruction = "你好" * 10000
             new_example = {
                 "system": system,
-                "instruction": instruction,
-                # "instruction": prompt.replace("{question}", question).replace("{requirement}", requirement).replace("{context}", context),
+                "instruction": prompt.replace("{question}", question).replace("{requirement}", requirement).replace("{context}", context),
                 "input": "",
                 "output": output,
                 "history": []
