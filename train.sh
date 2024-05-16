4c548f4596a7a54dbb44f271f6ef85037139263b
RealDuxy <paliss001@outlook.com>
2024-05-17 02:18:07 +0800

    1. 模版修改

--- train.sh
+++ train.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# 运行第一个训练命令
-CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch \
+# 使用 timeout 命令设置最长运行时间为3小时
+timeout 3h CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch \
     --config_file examples/accelerate/single_config.yaml \
-    src/train.py examples/train/0515_chatglm/exp1.yaml > log/chatglm_exp1_0515.log 2>&1
+    src/train.py examples/train/0515_chatglm/exp1.yaml > log/chatglm_exp1_0515.log 2>&1 &
 
-# 等待第一个命令完全结束
-wait
+# 等待第一个命令完成或超时
+wait $!
 
 # 运行第二个训练命令
 CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch \
     --config_file examples/accelerate/single_config.yaml \
     src/train.py examples/train/0513/exp1.yaml > log/qwen_exp1_0515.log 2>&1
-
-# 等待第二个命令完全结束
-wait
