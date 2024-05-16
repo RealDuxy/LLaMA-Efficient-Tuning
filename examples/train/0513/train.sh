f641267885232065eb2b5b2c42d9e506903b2391
RealDuxy <paliss001@outlook.com>
2024-05-17 01:10:38 +0800

    1. 模版修改

--- examples/train/0513/train.sh
+++ examples/train/0513/train.sh
@@ -8,16 +8,16 @@
 #  USE_MODELSCOPE_HUB=1CUDA_VISIBLE_DEVICES=2 python src/train.py examples/train/0513/exp3.yaml > log/exp3.log 2>&1
 #  USE_MODELSCOPE_HUB=1CUDA_VISIBLE_DEVICES=2 python src/train.py examples/train/0513/exp4.yaml > log/exp4.log 2>&1
 #) &
-echo "estimate gpu memory = 32413mb"
-echo "Start training exp2"
-CUDA_VISIBLE_DEVICES=0,1,2,3 python src/train.py examples/train/0513/exp2.yaml > log/exp2.log 2>&1
-echo "Start training exp3"
-CUDA_VISIBLE_DEVICES=0,1,2,3 python src/train.py examples/train/0513/exp3.yaml > log/exp3.log 2>&1
-echo "Start training exp4"
-CUDA_VISIBLE_DEVICES=0,1,2,3 python src/train.py examples/train/0513/exp4.yaml > log/exp4.log 2>&1
-echo "Start training exp1"
-CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/0513/exp1.yaml > log/exp1_0515.log 2>&1 &
-echo "training complete"
+#echo "estimate gpu memory = 32413mb"
+#echo "Start training exp2"
+#CUDA_VISIBLE_DEVICES=0,1,2,3 python src/train.py examples/train/0513/exp2.yaml > log/exp2.log 2>&1
+#echo "Start training exp3"
+#CUDA_VISIBLE_DEVICES=0,1,2,3 python src/train.py examples/train/0513/exp3.yaml > log/exp3.log 2>&1
+#echo "Start training exp4"
+#CUDA_VISIBLE_DEVICES=0,1,2,3 python src/train.py examples/train/0513/exp4.yaml > log/exp4.log 2>&1
+#echo "Start training exp1"
+#CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/0513/exp1.yaml > log/exp1_0515.log 2>&1 &
+#echo "training complete"
 
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch \
--- examples/train/0515_chatglm/train.sh
+++ examples/train/0515_chatglm/train.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-NPROC_PER_NODE=4
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run \
-    --nproc_per_node $NPROC_PER_NODE \
-    --nnodes 1 \
-    --standalone \
-    src/train.py examples/train/0515_chatglm/exp1.yaml
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run \
-    --nproc_per_node 4 \
-    --nnodes 1 \
-    --standalone \
-    src/train.py examples/train/0515_chatglm/exp1.yaml
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch \
-    --config_file examples/accelerate/single_config.yaml \
-    src/train.py examples/train/0515_chatglm/exp1.yaml
+#NPROC_PER_NODE=4
+#
+#CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run \
+#    --nproc_per_node $NPROC_PER_NODE \
+#    --nnodes 1 \
+#    --standalone \
+#    src/train.py examples/train/0515_chatglm/exp1.yaml
+#
+#CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run \
+#    --nproc_per_node 4 \
+#    --nnodes 1 \
+#    --standalone \
+#    src/train.py examples/train/0515_chatglm/exp1.yaml
+#
+#CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch \
+#    --config_file examples/accelerate/single_config.yaml \
+#    src/train.py examples/train/0515_chatglm/exp1.yaml
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch \
     --config_file examples/accelerate/single_config.yaml \
-    src/train.py examples/train/0515_chatglm/exp1.yaml > log/chatglm_exp1_0515.log 2>&1 &
+    src/train.py examples/train/0515_chatglm/exp1.yaml > log/chatglm_exp1_0515.log 2>&1
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch \
+    --config_file examples/accelerate/single_config.yaml \
+    src/train.py examples/train/0513/exp1.yaml > log/qwen_exp1_0515.log 2>&1
