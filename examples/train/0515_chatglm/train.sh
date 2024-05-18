c38a6ecabcf99aa3389f37afdf80c78c3f39f231
RealDuxy <paliss001@outlook.com>
2024-05-19 03:23:38 +0800

    1. comparison dataset 2. evaluation qwen

--- examples/train/0515_chatglm/train.sh
+++ examples/train/0515_chatglm/train.sh
@@ -22,7 +22,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch \
     --config_file examples/accelerate/single_config.yaml \
     src/train.py examples/train/0515_chatglm/exp1.yaml > log/chatglm_exp1_0515.log 2>&1 ; \
 
-#USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch \
+USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch \
     --config_file examples/accelerate/single_config.yaml \
     src/train.py examples/train/0515_chatglm/exp1_dpo.yaml > log/dpo_chatglm_exp1_0515.log 2>&1 &
 
