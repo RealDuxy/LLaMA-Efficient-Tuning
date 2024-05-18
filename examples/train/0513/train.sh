80b3fa585ee215f4b921042af0db2dd0122d9050
RealDuxy <paliss001@outlook.com>
2024-05-19 01:21:23 +0800

    1. comparison dataset 2. evaluation qwen

--- examples/train/0513/train.sh
+++ examples/train/0513/train.sh
@@ -28,3 +28,6 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch \
     --config_file examples/accelerate/single_config.yaml \
     src/train.py examples/train/0513/exp1_dpo.yaml > log/dpo_qwen_exp1_0515.log 2>&1 &
 
+USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
+    src/train.py examples/train/0513/exp1_dpo.yaml > log/dpo_qwen_exp1_0515.log 2>&1 &
+
