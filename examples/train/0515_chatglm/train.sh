15d9f05cbaa5d433385cbb7328b6deef00bd4112
RealDuxy <du.xi.yang.cool@gmail.com>
2024-05-19 22:46:44 +0800

    1. cls

--- examples/train/0515_chatglm/train.sh
+++ examples/train/0515_chatglm/train.sh
@@ -24,7 +24,11 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch \
 
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch \
     --config_file examples/accelerate/single_config.yaml \
-    src/train.py examples/train/0515_chatglm/exp1_dpo.yaml > log/dpo_chatglm_exp1_0519_small_lr.log 2>&1 &
+    src/train.py examples/train/0515_chatglm/exp1_dpo.yaml > log/dpo_chatglm_exp1_0519.log 2>&1 &
+
+USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch \
+    --config_file examples/accelerate/single_config.yaml \
+    src/train.py examples/train/0515_chatglm/exp2_dpo.yaml > log/dpo_chatglm_exp2_0519.log 2>&1 &
 
 
 
