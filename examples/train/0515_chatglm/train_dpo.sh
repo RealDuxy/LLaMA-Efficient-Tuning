6b37b4d1c863858feff22bf71f969c6b49e6609b
RealDuxy <paliss001@outlook.com>
2024-05-20 13:17:43 +0800

    1. comparison dataset 2. evaluation qwen

--- examples/train/0515_chatglm/train_dpo.sh
+++ examples/train/0515_chatglm/train_dpo.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch \
-    --config_file examples/accelerate/single_config.yaml \
-    src/train.py \
-    examples/train/0515_chatglm/exp2_dpo.yaml
-
-
-
+CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --config_file examples/accelerate/single_config.yaml src/train.py examples/train/0515_chatglm/exp2_dpo.yaml
