6e11b7766b4b7c650eea58079bee664378506b71
RealDuxy <du.xi.yang.cool@gmail.com>
2024-05-30 00:44:24 +0800

    1. cls

--- examples/train/0513/train.sh
+++ examples/train/0513/train.sh
@@ -31,6 +31,9 @@
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
     src/train.py examples/train/0513/exp1.yaml > log/sft_qwen_exp1_0527.log 2>&1 &
 
+  USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
+  src/train.py examples/train/0513/exp2.yaml > log/sft_qwen_exp2_0527.log 2>&1 &
+
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
     src/train.py examples/train/0513/exp1_dpo.yaml > log/dpo_qwen_exp1_0520.log 2>&1 &
 
@@ -39,8 +42,6 @@ USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
 
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
     src/train.py examples/train/0513/exp1_simpo.yaml > log/simpo_qwen_exp1_0529.log 2>&1 &
-
-sleep 5h
 
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
     src/train.py examples/train/0513/exp2_simpo.yaml > log/simpo_qwen_exp2_0529.log 2>&1 &
