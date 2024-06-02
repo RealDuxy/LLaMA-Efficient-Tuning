2c51cbd55d441893fa4667037aa0def63268bce1
RealDuxy <du.xi.yang.cool@gmail.com>
2024-06-03 00:31:38 +0800

    1. 添加comparison訓練數據

--- examples/train/0601/train_node2.sh
+++ examples/train/0601/train_node2.sh
@@ -1,2 +1,2 @@
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
-    src/train.py examples/train/0601/exp4_simpo.yaml > log/0601_qwen_rag_simpo_exp4.log 2>&1 &
+    src/train.py examples/train/0601/exp4_simpo.yaml > log/0601_qwen_rag_simpo_exp4.log 2>&1
