882a375e1b19070f62ae0ab5cbd8f3ff4fcdacc6
RealDuxy <paliss001@outlook.com>
2024-06-03 18:35:18 +0800

    1. qwen7b -full tuning

--- examples/train/0603-qwen-full/train_full_qwen.sh
+++ examples/train/0603-qwen-full/train_full_qwen.sh
@@ -1,3 +1,7 @@
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0,1,2,3 nohup accelerate launch src/train.py examples/train/0603-qwen-full/exp1.yaml > log/0603_qwen-7b-rag-exp1.log 2>&1
 
-#USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch src/train.py examples/train/0603-qwen-full/exp1.yaml
+USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun src/train.py examples/train/0603-qwen-full/exp1.yaml
+
+USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0,1,2,3 deepspeed --num_gpus=2 src/train.py examples/train/0603-qwen-full/exp1.yaml
+
+USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0,1,2,3 llamafactory-cli train examples/train/0603-qwen-full/exp1.yaml
