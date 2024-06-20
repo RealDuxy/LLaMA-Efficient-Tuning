690586997f19c40d7f4f5fa1d867c0493fb19c6f
RealDuxy <du.xi.yang.cool@gmail.com>
2024-06-21 00:59:19 +0800

    1. 添加comparison訓練數據

--- examples/train/0620/train_sft.sh
+++ examples/train/0620/train_sft.sh
@@ -3,8 +3,8 @@
 
 #USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/0620/qwen15-sft-exp1.yaml
 
-# 睡眠半小时
-sleep 1h
+## 睡眠半小时
+#sleep 1h
 
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
     src/train.py examples/train/0620/qwen15-sft-exp1.yaml > log/qwen15-sft-exp1.log 2>&1
