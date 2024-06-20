7ef983533bda0291f243ba066161ec82c20eac0b
RealDuxy <du.xi.yang.cool@gmail.com>
2024-06-21 01:30:40 +0800

    1.训练

--- data/dynamic_cot_trigger_rag/dynamic_cot_trigger_rag.py
+++ data/dynamic_cot_trigger_rag/dynamic_cot_trigger_rag.py
@@ -12,7 +12,7 @@ _LICENSE = ""
 _URL = "train_0619_dynamic_cot_trigger.json"
 # _URL = "eval_dynamic_cot_trigger.json"
 
-template = json.load(open("data/dynamic_cot_trigger_rag/template.json", "r", encoding="utf-8"))
+# template = json.load(open("data/dynamic_cot_trigger_rag/template.json", "r", encoding="utf-8"))
 
 class DynamicCoTDataset(datasets.GeneratorBasedBuilder):
     VERSION = datasets.Version("0.0.0")
@@ -39,9 +39,11 @@ class DynamicCoTDataset(datasets.GeneratorBasedBuilder):
 
     def _generate_examples(self, filepath: str) -> Dict[int, Dict[str, Any]]:
         example_dataset = json.load(open(filepath, "r", encoding="utf-8"))
-        prompt_templates = deepcopy(template)
-        system = prompt_templates["history"][0]["content"]
-        prompt = prompt_templates["prompt"]
+        # prompt_templates = deepcopy(template)
+        # system = prompt_templates["history"][0]["content"]
+        # prompt = prompt_templates["prompt"]
+        system = "你是安安，是由中国平安人寿保险有限公司开发和提供的智能保险专家。你的任务是根据检索到的资料回答用户的问题。"
+        prompt = "**检索到的资料**：\n```\n{context}\n```\n**问题**：{question}{requirement}"
         for key, example in enumerate(example_dataset):
             question = example["question"]
             if question[-1] not in ["？", "。", "！", "?", ".", "!"]:
@@ -52,7 +54,7 @@ class DynamicCoTDataset(datasets.GeneratorBasedBuilder):
             random.shuffle(context)
             context = "\n\n".join(context)
             is_positive = example["is_positive"]
-            if not is_positive: continue
+            # if not is_positive: continue
             new_example = {
                 "system": system,
                 "instruction": prompt.replace("{question}", question).replace("{requirement}", requirement).replace("{context}", context),
--- main.py
+++ main.py
@@ -6,27 +6,36 @@
 @Email   : du.xi.yang@qq.com
 @Software: PyCharm
 """
-import json
+from data.instruction_only_rag.instruction_only_rag import InstructionOnlyDataset, _URL
 
+# import json
+#
+# from data.instruction_only_rag.instruction_only_rag import InstructionOnlyDataset
+# from data.qwen_rag_comparison.qwen_rag_comparison import QwenRAGComparisonDataset, _URL
+# # dataset = QwenRAGComparisonDataset()
+# # comparison_datas = [new_example
+# #                     for key, new_example in dataset._generate_examples(filepaths=_URL)]
+# #
+# # json.dump(comparison_datas,
+# #           open("qwen_rag_dpo.json", "w", encoding="utf-8"),
+# #           ensure_ascii=False, indent=4)
+#
+# # from data.chatglm_rag_comparison.chatglm_rag_comparison import ChatGLMRAGComparisonDataset, _URL
+# # dataset = ChatGLMRAGComparisonDataset()
+#
 # from data.qwen_rag_comparison.qwen_rag_comparison import QwenRAGComparisonDataset, _URL
 # dataset = QwenRAGComparisonDataset()
+#
 # comparison_datas = [new_example
 #                     for key, new_example in dataset._generate_examples(filepaths=_URL)]
 #
+# print(len(comparison_datas))
 # json.dump(comparison_datas,
-#           open("qwen_rag_dpo.json", "w", encoding="utf-8"),
+#           open("data/qwen_rag_comparison_0527_10p.json", "w", encoding="utf-8"),
 #           ensure_ascii=False, indent=4)
 
-# from data.chatglm_rag_comparison.chatglm_rag_comparison import ChatGLMRAGComparisonDataset, _URL
-# dataset = ChatGLMRAGComparisonDataset()
 
-from data.qwen_rag_comparison.qwen_rag_comparison import QwenRAGComparisonDataset, _URL
-dataset = QwenRAGComparisonDataset()
+dataset = InstructionOnlyDataset()
+for key, new_example in dataset._generate_examples(filepath=_URL):
+    print()
 
-comparison_datas = [new_example
-                    for key, new_example in dataset._generate_examples(filepaths=_URL)]
-
-print(len(comparison_datas))
-json.dump(comparison_datas,
-          open("data/qwen_rag_comparison_0527_10p.json", "w", encoding="utf-8"),
-          ensure_ascii=False, indent=4)
