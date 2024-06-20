43b53c744ab25f68f8f377af43fd28a2f1bb5773
RealDuxy <paliss001@outlook.com>
2024-06-20 19:35:44 +0800

    1. 新实验

--- examples/train/0620/train_sft.sh
+++ examples/train/0620/train_sft.sh
@@ -1,7 +1,11 @@
 #USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
 #    src/train.py examples/train/0620/qwen2-sft-exp1.yaml > log/qwen2-sft-exp1.log 2>&1
 
-USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/0620/qwen15-sft-exp1.yaml
+#USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/0620/qwen15-sft-exp1.yaml
+
+# 睡眠1小时
+sleep 1h
+
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
     src/train.py examples/train/0620/qwen15-sft-exp1.yaml > log/qwen15-sft-exp1.log 2>&1 &
 
