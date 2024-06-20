84d31108046a2f094ff6212dfe579841f1e87071
RealDuxy <paliss001@outlook.com>
2024-06-20 18:31:38 +0800

    1. 新实验

--- examples/train/0620/train_sft.sh
+++ examples/train/0620/train_sft.sh
@@ -2,4 +2,4 @@
 #    src/train.py examples/train/0620/qwen2-sft-exp1.yaml > log/qwen2-sft-exp1.log 2>&1
 
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
-    src/train.py examples/train/0620/qwen15-sft-exp1.yaml > log/qwen15-sft-exp1.log 2>&1
+    src/train.py examples/train/0620/qwen15-sft-exp1.yaml > log/qwen15-sft-exp1.log 2>&1 &
