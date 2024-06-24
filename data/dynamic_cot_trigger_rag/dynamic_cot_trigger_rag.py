87af9ad2b901ccd447590b192e4f8a6e4e34060e
RealDuxy <paliss001@outlook.com>
2024-06-24 10:54:40 +0800

    1. 添加测试集0620

--- data/dynamic_cot_trigger_rag/dynamic_cot_trigger_rag.py
+++ data/dynamic_cot_trigger_rag/dynamic_cot_trigger_rag.py
@@ -9,10 +9,25 @@ _DESCRIPTION = "RAG dataset with dynamic CoT trigger"
 _CITATION = ""
 _HOMEPAGE = ""
 _LICENSE = ""
-_URL = "train_0619_dynamic_cot_trigger.json"
-# _URL = "eval_dynamic_cot_trigger.json"
 
-# template = json.load(open("data/dynamic_cot_trigger_rag/template.json", "r", encoding="utf-8"))
+_URL = "./"
+_URLS = {
+    "train": [
+        _URL + "train_0619_dynamic_cot_trigger.json",
+        # _URL + "train_0619_fix_cot_trigger.json",
+        _URL + "train_0619_instruction_only.json"
+    ],
+    "test": [
+        _URL + "eval_dynamic_cot_trigger.json",
+        _URL + "eval_dynamic_cot_trigger_all_negative.json",
+        _URL + "eval_fix_cot_trigger.json",
+        _URL + "eval_fix_cot_trigger_all_negative.json",
+        _URL + "eval_instruction_only.json",
+        _URL + "eval_instruction_only_all_negative.json"
+    ],
+}
+
+template = json.load(open("data/dynamic_cot_trigger_rag/template_0620.json", "r", encoding="utf-8"))
 
 class DynamicCoTDataset(datasets.GeneratorBasedBuilder):
     VERSION = datasets.Version("0.0.0")
@@ -32,32 +47,39 @@ class DynamicCoTDataset(datasets.GeneratorBasedBuilder):
             citation=_CITATION
         )
 
-    def _split_generators(self, dl_manager: datasets.DownloadManager) -> List[datasets.SplitGenerator]:
-        file_path = dl_manager.download(_URL)
-        return [datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": file_path})]
+    def _split_generators(self, dl_manager: datasets.DownloadManager):
+        file_path = dl_manager.download_and_extract(_URLS)
+        return [
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepaths": file_path["train"]}),
+            datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={"filepaths": file_path["test"]}),
+        ]
 
-    def _generate_examples(self, filepath: str) -> Dict[int, Dict[str, Any]]:
-        example_dataset = json.load(open(filepath, "r", encoding="utf-8"))
-        print(f"reading dataset: {filepath}")
+    def _generate_examples(self, filepaths: List[str]):
+        key = 0
+        for filepath in filepaths:
 
-        for key, example in enumerate(example_dataset):
-            system = "你是安安，是由中国平安人寿保险有限公司开发和提供的智能保险专家。你的任务是根据检索到的资料回答用户的问题。"
-            prompt = "**检索到的资料**：\n```\n{context}\n```\n**问题**：{question}{requirement}"
-            question = example["question"]
-            if question[-1] not in ["？", "。", "！", "?", ".", "!"]:
-                question += "？"
-            requirement = example["requirement"].replace("\n", "")
-            output = example["output"]
-            context = example["contexts"]
-            random.shuffle(context)
-            context = "\n\n".join(context)
-            is_positive = example["is_positive"]
-            # if not is_positive: continue
-            new_example = {
-                "system": system,
-                "instruction": prompt.replace("{question}", question).replace("{requirement}", requirement).replace("{context}", context),
-                "input": "",
-                "output": output,
-                "history": []
-            }
-            yield key, new_example
+            example_dataset = json.load(open(filepath, "r", encoding="utf-8"))
+            print(f"reading dataset: {filepath}")
+            for filepath in filepaths:
+                with open(filepath, "r", encoding="utf-8") as f:
+                    for row in f:
+                        example = json.loads(row)
+                        system = template["history"][0]["content"]
+                        prompt = template["prompt"]
+                        question = example["question"]
+                        if question[-1] not in ["？", "。", "！", "?", ".", "!"]:
+                            question += "？"
+                        requirement = example["requirement"].replace("\n", "")
+                        output = example["output"]
+                        context = example["contexts"]
+                        random.shuffle(context)
+                        context = "\n\n".join(context)
+                        new_example = {
+                            "system": system,
+                            "instruction": prompt.replace("{question}", question).replace("{requirement}", requirement).replace("{context}", context),
+                            "input": "",
+                            "output": output,
+                            "history": []
+                        }
+                        yield key, new_example
+                        key += 1
