0f420616d5b6d68f8ae0229d4a90af5398f83894
RealDuxy <du.xi.yang.cool@gmail.com>
2024-07-23 01:23:02 +0800

    new version rag sft

--- examples/train/0722/train_sft.sh
+++ examples/train/0722/train_sft.sh
@@ -11,3 +11,10 @@ USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
 
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
     src/train.py examples/train/0620/qwen15-sft-exp4.yaml > log/qwen15-sft-exp4.log 2>&1
+
+
+USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
+    src/train.py examples/train/0722/qwen15-sft-exp1.yaml > log/qwen15-sft-0722-exp1.log 2>&1
+
+
+USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/0722/qwen15-sft-exp1.yaml
--- src/llamafactory/train/sft/metric.py
+++ src/llamafactory/train/sft/metric.py
@@ -1,6 +1,6 @@
 import re
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Dict, Optional
+from typing import TYPE_CHECKING, Dict, Optional, Sequence, Union, Tuple
 
 import numpy as np
 import torch
