b830275cec88840e97ca4a6f9fee23dc72a9cae3
RealDuxy <du.xi.yang.cool@gmail.com>
2024-07-29 02:45:47 +0800

    new version rag sft

--- examples/train/RAG_research_training/train_sft.sh
+++ examples/train/RAG_research_training/train_sft.sh
@@ -7,8 +7,7 @@
 #sleep 1h
 
 
+USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/RAG_research_training/train.yaml
 
 
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python src/train.py examples/train/RAG_research_training/train.yaml > log/llama3-alpaca-lora.log 2>&1 &
-
-USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python src/train.py examples/train/0722/qwen15-stage1-exp2.yaml > log/qwen15-stage1-0722-exp2.log 2>&1
