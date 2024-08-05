661c83db17c6192992868b572d1bc4a2c38a01ce
RealDuxy <du.xi.yang.cool@gmail.com>
2024-08-05 17:12:18 +0800

    new version rag sft

--- examples/train/RAG_research_training/train_sft.sh
+++ examples/train/RAG_research_training/train_sft.sh
@@ -7,13 +7,13 @@
 #sleep 1h
 
 
-#CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/RAG_research_training/train.yaml
+#CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/RAG_research_training/train_alpaca_instruct.yaml
 #
 #CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/RAG_research_training/train_rag.yaml
 #
 #CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/RAG_research_training/train_rag_sft.yaml
 
-#CUDA_VISIBLE_DEVICES=0 nohup python src/train.py examples/train/RAG_research_training/train.yaml > log/llama3-alpaca-lora.log 2>&1 &
+#CUDA_VISIBLE_DEVICES=0 nohup python src/train.py examples/train/RAG_research_training/train_alpaca_instruct.yaml > log/llama3-alpaca-lora.log 2>&1 &
 
 CUDA_VISIBLE_DEVICES=0 nohup python src/train.py examples/train/RAG_research_training/train_rag_sft.yaml > log/0729-llama3-8b-lora-alpaca-nq-seq.log 2>&1
 
