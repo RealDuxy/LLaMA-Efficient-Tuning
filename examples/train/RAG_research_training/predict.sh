8fc54d262c92b42b6b05bc10c4e0bb31b9c6bf4d
RealDuxy <du.xi.yang.cool@gmail.com>
2024-08-05 16:50:26 +0800

    new version rag sft

--- examples/train/RAG_research_training/predict.sh
+++ examples/train/RAG_research_training/predict.sh
@@ -1,4 +1,4 @@
 USE_MODELSCOPE_HUB=1 CUDA_VISIBLE_DEVICES=0 nohup python \
     src/train.py examples/train/0620/qwen15-sft-exp3-predict.yaml > log/qwen15-sft-exp3-predict.log 2>&1 &
 
-CUDA_VISIBLE_DEVICES=0 python src/train.py eexamples/train/RAG_research_training/llama3-predict.yaml
+CUDA_VISIBLE_DEVICES=0 python src/train.py examples/train/RAG_research_training/llama3-predict.yaml
