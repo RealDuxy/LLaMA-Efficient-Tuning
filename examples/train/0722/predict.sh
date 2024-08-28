a796dba1ea6c098cb9734fd3804ee10f0023eebc
RealDuxy <paliss001@outlook.com>
2024-08-28 16:26:52 +0800

    1. add qwen vllm infer

--- examples/train/0722/predict.sh
+++ examples/train/0722/predict.sh
@@ -3,3 +3,6 @@ python src/train.py examples/train/0722/qwen15-sft-stage12-predict.yaml
 CUDA_VISIBLE_DEVICES=0 nohup python src/train.py examples/train/0722/qwen15-sft-stage12-predict.yaml > log/qwen15-sft-stage2.log 2>&1
 
 
+API_PORT=8000 llamafactory-cli api examples/inference/qwen15_vllm.yaml
+
+python src/api.py examples/inference/qwen15_vllm.yaml
