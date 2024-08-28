bbb7e51b9d60f2c5a2125161bbe4fc39561c34cc
RealDuxy <paliss001@outlook.com>
2024-08-28 15:00:13 +0800

    1. predict script for rag training stage1/2

---
+++ examples/train/0722/predict.sh
@@ -0,0 +1,5 @@
+python src/train.py examples/train/0722/qwen15-sft-stage12-predict.yaml
+
+CUDA_VISIBLE_DEVICES=0 nohup python src/train.py examples/train/0722/qwen15-sft-stage12-predict.yaml > log/qwen15-sft-stage2.log 2>&1
+
+
