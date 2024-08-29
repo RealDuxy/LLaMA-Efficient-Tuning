b8a21acaa2a82f5cfe10da076e49d77069e90752
RealDuxy <du.xi.yang.cool@gmail.com>
2024-08-29 17:52:25 +0800

    1. 更新training script

--- examples/train/0722/local_vllm_predict.py
+++ examples/train/0722/local_vllm_predict.py
@@ -12,10 +12,16 @@ from vllm.lora.request import LoRARequest
 from vllm_wrapper import vLLMWrapper
 
 model_path = "/root/autodl-tmp/qwen/Qwen1_5-14B-Chat-GPTQ-Int4"
+
+lora_request = LoRARequest("stage2",
+                           1,
+                           "/root/autodl-tmp/checkpoints/qwen/0722_qwen15_rag_sft_exp1")
+
 vllm_model = vLLMWrapper(model_path,
                          dtype="float16",
                          tensor_parallel_size=1,
-                         gpu_memory_utilization=0.9)
+                         gpu_memory_utilization=0.9,
+                         lora_request=lora_request)
 
 history=None
 while True:
